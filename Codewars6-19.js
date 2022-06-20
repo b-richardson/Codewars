98ce262085e3ba2389a80c4ed685c1981b905f86
b-richardson <97001156+b-richardson@users.noreply.github.com>
2022-06-20 17:17:41 -0400

    fizz buzz practice

--- Codewars6-19.js
+++ Codewars6-19.js
@@ -4,3 +4,18 @@ function solution(str){
     return str.split('').reverse().join('');
  }
  
+
+
+ var fizzBuzz = function(n) {
+    for(let i = 1; i < n.length; i++){
+        if(i % 3){
+           console.log('fizz')
+           }else if(i % 5){
+               console.log('buzz')     
+           }else if((i % 3 === 0) && (i % 5 === 0)){
+               console.log('fizz buzz')
+           }else{
+               console.log(i)
+           }
+    }
+};
