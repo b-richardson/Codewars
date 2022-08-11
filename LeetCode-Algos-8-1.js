b8d083e7ed43778ca74bd1f2ca6e0c0dd2f944ea
b-richardson <97001156+b-richardson@users.noreply.github.com>
2022-08-11 19:21:24 -0400

    More Algo Practice!

---
+++ LeetCode-Algos-8-1.js
@@ -0,0 +1,25 @@
+// Move Zeroes
+var moveZeroes = function(nums) {
+    let numOfZeroes = 0
+    
+    // nums.sort((a,b) => a - b)
+    
+    for (let i = 0; i < nums.length; i++ ){
+        console.log('cycle')
+        let isZero = nums[i] === 0
+        if(isZero){
+            numOfZeroes++
+            console.log('found a zero')
+            nums.splice(i,1)
+            i--
+            console.log('spliced')
+           } 
+    }
+    console.log(numOfZeroes)
+    for (let i = 0; i < numOfZeroes; i++){
+        console.log('push')
+        nums.push(0)
+    }
+    console.log(nums)
+    return (nums)
+};
